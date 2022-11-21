b8bff4fa874f5e8fe33667059d363a10b6415670
gevalinho <gevalinho1@gmail.com>
2022-11-21 11:57:16 +0100

    testing main

---
+++ test_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  main - executable
+ *  Return: 0;
+ */
+
+int main(void)
+{
+char *chr;
+
+chr = "H";
+_printf("Character: [%c]\n", chr);
+return (0);
+}
