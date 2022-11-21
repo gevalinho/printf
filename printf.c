28d148f1497805ce68c12a6c581584f7879f9e12
gevalinho <gevalinho1@gmail.com>
2022-11-21 10:38:48 +0100

    team work printf

--- printf.c
+++ printf.c
@@ -4,82 +4,85 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ *  _putchar_c writes a character (an unsigned char) specified by the argument char to stdout.
+ *  @c character agument
+ */
+
 void _putchar_c(char c)
 {
-    write(1, &c, 1);
+write(1, &c, 1);
 }
 
+/**
+ * _putchar writes a character for variadic functions
+ * @a aguement
+ */
 void _putchar(va_list a)
 {
-    char c;
-
-    c = va_arg(a, int);
-    write(1, &c, 1);
+char c;
+c = va_arg(a, int);
+write(1, &c, 1);
 }
 
 void print_str(va_list a)
 {
-    char *c;
-
-    c = va_arg(a, char *);
-    while (*c != '\0')
-    {
-        _putchar_c(*c++);
-    }
-}
-
-void print_int()
+char *c;
+c = va_arg(a, char *);
+while (*c != '\0')
 {
-    
+_putchar_c(*c++);
+}
 }
 
+void print_int(void)
+{ }
 
 /**
- *  * _printf - prints output according to a format.
- *   * @format: input string.
- *    * Return: int, number of characters printed,
- *     */
+ *  _printf - prints output according to a format.
+ *  @format: input string.
+ *  Return: int, number of characters printed,
+ */
 
 int _printf(const char *format, ...)
 {
-    va_list a;
-    int i, j, count;
+va_list a;
+int i, j, count;
 
-    cs_t cspec[] = {
-        {'c', _putchar},
-        {'s', print_str},
-        {'d', print_int},
-        {'i', print_int}
-    };
+cs_t cspec[] = {
+{'c', _putchar},
+{'s', print_str},
+{'d', print_int},
+{'i', print_int}
+};
 
-    if (format == NULL)
-        return (0);
+if (format == NULL)
+return (0);
 
-    i = j = count = 0;
-    va_start(a, format);
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            j = 0;
-            while (j < 4)
-            {
-                if (format[i + 1] == cspec[j].cs &&
-                    format[i + 1] != '%')
-                {
-                    cspec[j].f(a);
-                    i++;
-                }
-                j++;
-            }
-            i++;
-            _putchar_c(format[i]);
-        }
-        else
-            _putchar_c(format[i]);
-        count++;
-        i++;
-
-    }
-    return (count);
+i = j = count = 0;
+va_start(a, format);
+while (format[i])
+{
+if (format[i] == '%')
+{
+j = 0;
+while (j < 4)
+{
+if (format[i + 1] == cspec[j].cs &&
+format[i + 1] != '%')
+{
+cspec[j].f(a);
+i++;
+}
+j++;
+}
+i++;
+_putchar_c(format[i]);
+}
+else
+_putchar_c(format[i]);
+count++;
+i++;
+}
+return (count);
 }
