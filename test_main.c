cc53da4525c053068ef5da5f31bf39034cdf3285
gevalinho <gevalinho1@gmail.com>
2022-11-22 09:30:17 +0100

    prinf group project

---
+++ test_main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - testing printf function
+ *
+ * Return: always 0;
+ *
+ */
+
+int main(void)
+{
+char *chr;
+
+chr = "H";
+_printf("Character: [%c]\n", chr);
+return (0);
+}
